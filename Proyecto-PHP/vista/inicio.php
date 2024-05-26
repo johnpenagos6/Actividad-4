5200309b7b47ad08dd761679839a462a1c13ed6c
john.penagos.contractor <john.penagos.contractor@bbva.com>
2024-05-25 23:45:08 -0500

    feat: se realiza la actualizacion a la HU 1 escrita en el trabajo

--- Proyecto-PHP/vista/inicio.php
+++ Proyecto-PHP/vista/inicio.php
@@ -38,6 +38,27 @@
     <span class="site-heading-upper text-primary mb-3">CLUB DEPORTIVO REMO LA ARAÑA</span>
   </h1>
 
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">CLUB DEPORTIVO REMO LA ARAÑA</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item active px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="vista/calendario.php">calendario</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
   <section class="page-section clearfix">
     <div class="grid-container">
